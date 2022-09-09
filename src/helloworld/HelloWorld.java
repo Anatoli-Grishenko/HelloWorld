b7e6294949289a2ac1f5dbd75ba3b148803ffe84
Anatoli Grishenko <Anatoli.Grishenko@gmail.com>
2022-09-10 01:00:13 +0200

    Last minute edits

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -6,9 +6,21 @@ import appboot.LARVABoot;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        LARVABoot boot = new LARVABoot();
-        boot.Boot("150.214.190.126", 1099);
-        boot.launchAgent("Smith", AgentLARVA.class);
+        // Basic JADE boot (console)
+        JADEBoot boot=new JADEBoot();
+        // LARVA boot (GUI-based)
+//        LARVABoot boot = new LARVABoot();
+        
+        // In both cases, Boot( ) creates a container for future agents
+        // Should JADE be running in local
+        boot.Boot("localhost", 1099);
+        // Otherwise our server always run JADE  ;-)
+        // boot.Boot("150.214.190.126", 1099);
+        
+        // Create the agent and executes it
+        boot.launchAgent("Smith", AgentJADE.class);
+        
+        // Closes the container and exits
         boot.WaitToShutDown();
     }
     
