2e23658fc38bd7db6559f7465937fa3c1ffaf1c1
Anatoli Grishenko <Anatoli.Grishenko@gmail.com>
2022-09-13 09:55:14 +0200

    Last minute

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -6,12 +6,15 @@ import appboot.LARVABoot;
 public class HelloWorld {
 
     public static void main(String[] args) {
+//        bootJADE();
+        bootLARVA();
+    }
+    
+    public static void bootJADE() {
         // Basic JADE boot (console)
         JADEBoot boot=new JADEBoot();
-        // LARVA boot (GUI-based)
-//        LARVABoot boot = new LARVABoot();
         
-        // In both cases, Boot( ) creates a container for future agents
+        // Boot( ) creates a container for future agents
         // Should JADE be running in local
         boot.Boot("localhost", 1099);
         // Otherwise our server always run JADE  ;-)
@@ -20,11 +23,29 @@ public class HelloWorld {
         
         // Create the agent and executes it
         boot.launchAgent("Smith", AgentJADE.class);
+
+        // Closes the container and exits
+        boot.WaitToShutDown();
+        
+    }
+    public static void bootLARVA() {
+        // LARVA boot (GUI-based)
+        LARVABoot boot = new LARVABoot();
+        
+        // Boot( ) creates a container for future agents
+        // Should JADE be running in local
+        boot.Boot("localhost", 1099);
+        // Otherwise our server always run JADE  ;-)
+        // boot.Boot("isg2.ugr.es", 1099);  // Our server is isg2
+        // boot.Boot("150.214.190.126", 1099);  // Should there be problems with DNS
+        
+        // Create the agent and executes it
 //        boot.launchAgent("Smith", AgentLARVA.class);
-//        boot.launchAgent("Smith", AgentLARVAFull.class);
+        boot.launchAgent("Smith", AgentLARVAFull.class);
         
         // Closes the container and exits
         boot.WaitToShutDown();
+        
     }
     
 }
