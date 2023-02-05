53db197e780cfb2329d2a4aa95300297018e7aab
Anatoli Grishenko <Anatoli.Grishenko@gmail.com>
2023-02-05 12:33:07 +0100

    Profiler done

--- src/helloworld/AgentIncrementalProfiler.java
+++ src/helloworld/AgentIncrementalProfiler.java
@@ -16,14 +16,14 @@ import tools.TimeHandler;
  *
  * @author Anatoli Grishenko <Anatoli.Grishenko@gmail.com>
  */
-public class AgentProfiler extends AgentLARVAFull {
+public class AgentIncrementalProfiler extends AgentLARVAFull {
 
     public static final int MAXSIZE_BYTES = 1024 * 1024,
             INITIALSIZE_BYTES = 512,
-            SERIESIZE = 10;
+            SERIESIZE = 1;
 
     protected String service = "NETMONITOR", Payload, myNetworkMonitor, label, label2, stime;
-    protected int payloadSize, serie;
+    protected int payloadSize, serie = 0;
     protected AID aidNetMon;
     protected boolean zip = false;
 
@@ -41,17 +41,39 @@ public class AgentProfiler extends AgentLARVAFull {
             myNetworkMonitor = DFGetAllProvidersOf(service).get(0);
             aidNetMon = new AID(myNetworkMonitor, AID.ISLOCALNAME);
         }
-        activateMyCPUProfiler("AGENT-CPU.tsv");
-        activateMyNetworkProfiler("AGENT-NETWORK.tsv");
-        serie = 1;
+        activateMyCPUProfiler(getLocalName()+"-CPU");
+        activateMyNetworkProfiler(getLocalName()+"-NETWORK");
         payloadSize = INITIALSIZE_BYTES;
+        serie = 0;
     }
-    
- @Override
+
+    @Override
+    public void preExecute() {
+        super.preExecute();
+        lastCookie.setSerie(serie);
+    }
+
+    @Override
     public void Execute() {
-        getMyCPUProfiler().setSeries(myStatus.name()+(E.getCurrentMission()!= null? ","+E.getCurrentGoal():""));
-        getMyNetworkProfiler().setSeries(myStatus.name()+(E.getCurrentMission()!= null? ","+E.getCurrentGoal():""));
+        getMyCPUProfiler().setSeries(myStatus.name() + (E.getCurrentMission() != null ? "," + E.getCurrentGoal() : ""));
+        getMyNetworkProfiler().setSeries(myStatus.name() + (E.getCurrentMission() != null ? "," + E.getCurrentGoal() : ""));
         super.Execute();
+    }
+
+    @Override
+    public void postExecute() {
+        if (myStatus == Status.SOLVEPROBLEM) {
+            if (serie < 20) {
+                serie++;
+                if (serie == 11) {
+                    payloadSize = INITIALSIZE_BYTES;
+                } else {
+                    payloadSize *= 2;
+                }
+            }
+        }
+        super.postExecute();
+
     }
 
     @Override
@@ -60,8 +82,7 @@ public class AgentProfiler extends AgentLARVAFull {
         label = "Profiling Serie " + serie + " " + payloadSize;
         System.out.println(label);
         for (int serierep = 1; serierep <= SERIESIZE; serierep++) {
-            lastCookie = new NetworkCookie();
-            lastCookie.setSerie(serie);
+//            lastCookie = new NetworkCookie();
             lastCookie.setSize(payloadSize);
             zip = serie > 10;
             lastCookie.setZipped(zip);
@@ -70,36 +91,16 @@ public class AgentProfiler extends AgentLARVAFull {
             outbox.addReceiver(aidNetMon);
             outbox.setContent("");
             outbox.setReplyWith(label);
-            lastCookie.settUpstream(TimeHandler.Now());
+            lastCookie.settUpstream(TimeHandler.NetNow());
             outbox = Profiler.injectProfiler(outbox, lastCookie);
             LARVAsend(outbox);
             inbox = LARVAblockingReceive();
-//            stime = TimeHandler.Now();
-//            if (Profiler.isProfiler(inbox)) {
-//                nc = Profiler.extractProfiler(inbox);
-//                lastCookie.settReceive(stime);
-//                label = "PING\t" + nc.getSerie() + "\tSIZE\t" + payloadSize + "\tPAYLOAD\t" + inbox.getContent().length() + "\tZIP\t" + zip + "\t";
-//                label2 = "UPSTREAM\t" + nc.getLatencyUp()
-//                        + "\tSERVER\t" + nc.getLatencyServer()
-//                        + "\tDOWNSTREAM\t" + nc.getLatencyDown();
-//                getMyNetworkProfiler().profileThis(label, label2, () -> {
-//                    System.out.println(label + "" + label2);
-//                });
-//            }
         }
         if (serie < 20) {
-            serie++;
-            if (serie == 11) {
-                payloadSize = INITIALSIZE_BYTES;
-            } else {
-                payloadSize *= 2;
-            }
-            lastCookie=null;
             return Status.SOLVEPROBLEM;
         } else {
             return Status.CLOSEPROBLEM;
         }
     }
-    
 
 }
--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -11,47 +11,50 @@ public class HelloWorld {
 //        bootJADE();
         bootLARVA();
     }
-    
+
     public static void bootJADE() {
         // Basic JADE boot (console)
-        JADEBoot boot=new JADEBoot();
-        
+        JADEBoot boot = new JADEBoot();
+
         // Boot( ) creates a container for future agents
         // Should JADE be running in local
         boot.Boot(SwingTools.inputLine("Host"), 1099);
         // Otherwise our server always run JADE  ;-)
 //         boot.Boot("isg2.ugr.es", 1099);  // Our server is isg2
 //         boot.Boot("150.214.190.126", 1099);  // Should there be problems with DNS
-        
+
         // Create the agent and executes it
-        boot.launchAgent("Smith-"+getHexaKey(4), AgentJADE.class);
+        boot.launchAgent("Smith-" + getHexaKey(4), AgentJADE.class);
 
         // Closes the container and exits
         boot.WaitToShutDown();
-        
+
     }
 
     public static void bootLARVA() {
         // LARVA boot (GUI-based)
         LARVABoot boot = new LARVABoot();
-        
+        int nProfilers = 8;
         // Boot( ) creates a container for future agents
         // Should JADE be running in local
 //        boot.Boot("localhost", 1099);
-        boot.Boot(boot.inputLine("Host"), 1099);
+//        boot.Boot(boot.inputLine("Host"), 1099);
+        boot.Boot(SwingTools.inputSelect("Please select sever", new String[]{"localhost", "isg2.ugr.es", ""}, ""), 1099);
         // Otherwise our server always run JADE  ;-)
 //         boot.Boot("isg2.ugr.es", 1099);  // Our server is isg2
         // boot.Boot("150.214.190.126", 1099);  // Should there be problems with DNS
-        
+
         // Create the agent and executes it
 //        boot.launchAgent("Smith-"+getHexaKey(4), AgentLARVA.class);
 //        boot.launchAgent("Smith-"+getHexaKey(4), AgentLARVAFull.class);
 //        boot.launchAgent("Smith-"+getHexaKey(4), AgentDialogicFull.class);
-        boot.launchAgent("StudentProfiler", AgentProfiler.class);
-        
+        for (int i = 0; i < nProfilers; i++) {
+            boot.launchAgent("SP" + i, AgentLinearProfiler.class);
+        }
+
         // Closes the container and exits
         boot.WaitToShutDown();
-        
+
     }
-    
+
 }
